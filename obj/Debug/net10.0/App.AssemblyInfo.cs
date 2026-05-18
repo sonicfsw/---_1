be927cafd7eee38a33a73c5cc84f2c90af4aa507
Sofia Balachkova <iamsophieiam@yandex.ru>
2026-05-18 09:04:43 +0700

    Добавлены обработчик ошибок и возможность ввода с клавиатуры

--- obj/Debug/net10.0/App.AssemblyInfo.cs
+++ obj/Debug/net10.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80684546b4e2da3f9b1480848772d9b71d14efbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
